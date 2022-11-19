1e01e538a3e76c6e7c9ed0a89f9ad13abcc2d8ec
Manish Kunwar <ranaxmond@gmail.com>
2022-11-19 22:16:17 +0545

    removed venurabilites

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -66,12 +66,12 @@ const ProjectList = () => {
                                     Contact me
                                 </button>
                                 <div className='rounded-full bg-white dark:bg-gray-800 flex flex-row gap-x-4 p-3'>
-                                    <a href='https://github.com/myan-ish/' target={"_blank"}>
+                                    <a href='https://github.com/myan-ish/'>
                                         <DiGithubBadge size={24} />
                                     </a>
                                 </div>
                                 <div className='rounded-full bg-white dark:bg-gray-800 flex flex-row gap-x-4 p-3'>
-                                    <a href='https://www.linkedin.com/in/manish-kunwar/' target={"_blank"}>
+                                    <a href='https://www.linkedin.com/in/manish-kunwar/'>
                                         <FaLinkedin size={24} />
                                     </a>
                                 </div>
@@ -112,12 +112,12 @@ const ProjectList = () => {
                                 Contact me
                             </button>
                             <div className='rounded-full bg-white dark:bg-gray-800 flex flex-row gap-x-4 p-3'>
-                                <a href='https://github.com/myan-ish/' target={"_blank"}>
+                                <a href='https://github.com/myan-ish/'>
                                     <DiGithubBadge size={24} />
                                 </a>
                             </div>
                             <div className='rounded-full bg-white dark:bg-gray-800 flex flex-row gap-x-4 p-3'>
-                                <a href='https://www.linkedin.com/in/manish-kunwar/' target={"_blank"}>
+                                <a href='https://www.linkedin.com/in/manish-kunwar/'>
                                     <FaLinkedin size={24} />
                                 </a>
                             </div>
